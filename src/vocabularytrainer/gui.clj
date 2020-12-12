35f9ee6337a729b0ca6c0fbd4a1bff12916e413a
OgreBelly <ogrebelly@protonmail.com>
2020-12-12 12:27:09 +0000

    some views

--- src/vocabularytrainer/gui.clj
+++ src/vocabularytrainer/gui.clj
@@ -1,10 +1,37 @@
 (ns vocabularytrainer.gui
   (:require [strigui.core :as gui]))
 
+
+(def current-items (atom []))
+
+(defn clear-screen []
+  (doall (map (fn [item] (gui/remove! item) (println "remove")) @current-items)))
+
+(defn show-list []
+  (clear-screen)
+  (gui/update! "title" :value "View")
+  (gui/label "voc1" "blabla" {:x 100 :y 100 :color [:black]})
+  (gui/label "voc2" "blablabla" {:x 200 :y 100 :color [:black]})
+  (reset! current-items ["voc1" "voc2"]))
+
+(defn show-menu-main []
+  (clear-screen)
+  (gui/update! "title" :value "Vocabulary Trainer")
+  (gui/button "menu-practice" "Practice" {:x 220 :y 115 :color [:back :black] 
+                                                  :min-width 150})
+  (gui/button "menu-add" "Add" {:x 220 :y 165 :color [:back :black] 
+                                              :min-width 150})
+  (gui/button "menu-view" "View" {:x 220 :y 215 :color [:back :black] 
+                                                :min-width 150})
+  (reset! current-items ["menu-practice" "menu-add" "menu-view"])
+  (gui/update! "menu-view" [:events :mouse-clicked] (fn [wdg] (show-list))))
+
 (defn build-main
   []
   (gui/window 600 600)
-  (gui/label "title" "Welcome to Strigui" {:x 300 :y 50 :color [:black] :align [:center]})
-  (gui/label "question" "test" {:x 150 :y 150 :color [:black] :align [:left]})
-  (gui/input "answer" "" {:x 150 :y 200 :color [:white :black] :min-width 300})
-  (gui/stacks "progress" '(10 0 0 0 0) {:x 100 :y 300}))
+  (gui/label "title" "Vocabulary Trainer" {:x 300 :y 50 
+                                            :color [:black] 
+                                            :align [:center]
+                                            :font-size 20})
+  (show-menu-main))
+
